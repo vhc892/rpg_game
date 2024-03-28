5e1ee5f9f6633e3a14c89146e1da33820045221f
Vũ Huy Công <huycong892@gmail.com>
2024-03-28 12:54:19 +0700

    first commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum State {
+        Roaming
+    }
+    private State state;
+    private EnemyPathfinding enemyPathfinding;
+
+    private void Awake()
+    {
+        enemyPathfinding = GetComponent<EnemyPathfinding>();
+        state = State.Roaming;
+    }
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+
+    private IEnumerator RoamingRoutine()
+    {
+        while (state == State.Roaming)
+        {
+            Vector2 roamPositon = GetRoamingPositon();
+            enemyPathfinding.MoveTo(roamPositon);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+    private Vector2 GetRoamingPositon()
+    {
+        return new Vector2(Random.Range(-1f, 1f) , Random.Range(-1f, 1f)).normalized;
+    }
+
+}
